0a1307591f6e4a97177e5cf47cfdb41853d6dd37
Christoph Strebin <christoph.strebin@freenet.ag>
2023-01-11 16:25:47 +0100

    ABRMS-4563 MS Docker Startskript: Heapdump bei OOM

--- actions/create-microservice-docker-image/home/run.sh
+++ actions/create-microservice-docker-image/home/run.sh
@@ -1,2 +1,21 @@
-#!/bin/sh
-exec java ${JAVA_OPTS} ${SPRING_OPTS} -jar ${SERVICE_NAME}.jar
+#!/usr/bin/env bash
+appInstance=$(hostname)
+heapDumpDir=dumps/${SERVICE_NAME}
+heapDumpPath=${heapDumpDir}/${appInstance}_${SERVICE_VERSION}_$(date +"%Y-%m-%dT%H%M%S").hprof
+
+mkdir -p -- "$heapDumpDir"
+
+# Paranoia: Falls $heapDumpPath existiert, auch noch PID ergänzen (vor dem Punkt).
+# (Falls die auch existiert, wird Java nichts schreiben.)
+heapDumpPath=$(f="$heapDumpPath" pid=$$ flock -- "$heapDumpDir" bash -c '
+	if [ -e "$f" ]; then
+		printf "%s\\n" "${f%.*}_${pid}.${f##*.}"
+	else
+		printf "%s\\n" "$f"
+	fi
+')
+
+exec java \
+	 -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError "-XX:HeapDumpPath=$heapDumpPath" \
+	 ${JAVA_OPTS} ${SPRING_OPTS} \
+	 -jar "${SERVICE_NAME}.jar"
