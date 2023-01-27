8370a6e790e4a4ef6e7a8d2277df6ab36c6c61e9
Carsten Bolte <carsten.bolte@freenet.ag>
2023-01-27 12:54:04 +0100

    ABRMS-4562 Verbesserungen f. Package-Download
    - Maven-Install-Option bei Initialize Workflow
    - Packages-Download-Skript angepasst
    - Package-Download-Action angepasst

--- actions/download-release-package/download.sh
+++ actions/download-release-package/download.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 # Download v. Artefakten aus GitHub-Packages
 # ben. Umgebungsvariablen:
-# ARTIFACT_ID, GROUP_ID, ORGANISATION, VERSION, TARGET_PATH, GITHUB_USER, TOKEN
+# ARTIFACT_ID, GROUP_ID, ORGANISATION, VERSION, TARGET_PATH, 
+# GITHUB_USER, TOKEN, SUFFIX, CLASSIFIER
 # benutzt sed, Maven
 
 set -e
 # settings.xml anpassen mit Parametern
 echo "konfiguriere settings.xml..."
-sed -iBAK "s/{ARTIFACT_ID}/$ARTIFACT_ID/g;s/{TOKEN}/$TOKEN/g;s/{GITHUB_USER}/$GITHUB_USER/g;s/{ORGANISATION}/$ORGANISATION/g" settings.xml
+sed -i "s/{PACKAGE_NAME}/$PACKAGE_NAME/g;s/{TOKEN}/$TOKEN/g;s/{GITHUB_USER}/$GITHUB_USER/g;s/{ORGANISATION}/$ORGANISATION/g" settings.xml
 # Artefakt herunterladen mit Maven
-echo "lade Artefakt über mvn herunter..."
-mvn --settings ./settings.xml dependency:copy -Dartifact="$GROUP_ID:$ARTIFACT_ID:$VERSION" -DoutputDirectory=./ -Dmdep.stripVersion=true
-# heruntergeladenes Artefakt verschieben
-mv $ARTIFACT_ID.jar $TARGET_PATH/$artifactId-$version.jar
+echo "lade Artefakt [GROUP_ID:ARTIFACT_ID:VERSION:PACKAGING:CLASSIFIER]=[$GROUP_ID:$ARTIFACT_ID:$VERSION:$SUFFIX:$CLASSIFIER] über mvn herunter..."
+if [[ "$CLASSIFIER" != "" ]]; then
+    mvn --settings ./settings.xml dependency:copy -Dartifact="$GROUP_ID:$ARTIFACT_ID:$VERSION:$SUFFIX:$CLASSIFIER" -DoutputDirectory=./ -Dmdep.stripVersion=true
+    # heruntergeladenes Artefakt verschieben
+    mv $ARTIFACT_ID-$CLASSIFIER.$SUFFIX $TARGET_PATH/$ARTIFACT_ID-$VERSION-$CLASSIFIER.$SUFFIX
+    ls $TARGET_PATH/$ARTIFACT_ID-$VERSION-$CLASSIFIER.$SUFFIX
+else 
+    mvn --settings ./settings.xml dependency:copy -Dartifact="$GROUP_ID:$ARTIFACT_ID:$VERSION:$SUFFIX" -DoutputDirectory=./ -Dmdep.stripVersion=true
+    # heruntergeladenes Artefakt verschieben
+    mv $ARTIFACT_ID.$SUFFIX $TARGET_PATH/$ARTIFACT_ID-$VERSION.$SUFFIX
+    ls $TARGET_PATH/$ARTIFACT_ID-$VERSION.$SUFFIX
+fi
 
