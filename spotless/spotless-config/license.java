f949d0a1adc03384fd3b1b9265d5d5ea2995ba54
Carsten Bolte <carsten.bolte@freenet.ag>
2023-09-25 10:37:20 +0200

    MCBS-4474 Action korrigiert

---
+++ spotless/spotless-config/license.java
@@ -0,0 +1,6 @@
+//#***************************************************************************
+//# freenet Technologie und Prozesse Source File: $FILE
+//# Copyright (c) 1996-$YEAR by freenet DLS GmbH
+//# All rights reserved.
+//#***************************************************************************
+
